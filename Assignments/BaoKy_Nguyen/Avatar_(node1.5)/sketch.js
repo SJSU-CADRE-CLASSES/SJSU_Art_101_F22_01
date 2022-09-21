0e90c663d393d0b8af19cdf15860a8ce9ff55b4c
BaoYky <baoky14@gmail.com>
2022-09-20 17:45:29 -0700

    1.5

--- Assignments/BaoKy_Nguyen/Avatar_(node1.5)/sketch.js
+++ Assignments/BaoKy_Nguyen/Avatar_(node1.5)/sketch.js
@@ -1,23 +1,17 @@
 let mic;
-
 let count = 0;
-
 let myMap = 0;
-
 let circleChange = false;
-
 let scaleChange = false;
-
 let colorChange = false;
 
-let falling = [];
+let ball = [];
 
-let colorful = ["#cdb4db","#ffc8dd","#8BC34A", "#f4a261", "#bde0fe", "#ffc8dd","#ffb4a2","#bbd0ff", "#d0f4de"   ];
+let colorful = ["#cdb4db","#ffc8dd","#8BC34A", "#f4a261", "#bde0fe", "#ffc8dd","#ffb4a2","#bbd0ff", "#d0f4de" ];
 
 let eyecolor = ["#030303","#EDF8E1", "#FAEDE2", "#FFFFFF" ];
 
-
-function setup() {   
+function setup(){   
   let cnv = createCanvas(400, 400);
   cnv.mousePressed(userStartAudio);
   textAlign(CENTER);
@@ -25,13 +19,12 @@ function setup() {
   mic.start();
   
 
-  for (let i = 0; i <= 9; i++){
-    falling[i] = new Ball
-   (random(width), random (height),random(1,30));
-  }
- // console.log(falling);
- //  console.log(falling.length);
-  
+  for (let i = 0; i <= 20; i++){  
+   ball[i] = new Ball
+   (random(width), random(height),random(4,10),random(10,40));
+     }
+ // xpos, ypos, speed, size
+ 
 }
 
 function draw() {
@@ -40,16 +33,15 @@ function draw() {
   let x = width/2;
   let y = height*3/10;
   
-     if (count<360){ count++
+  if (count<360){
+    count++
   }else if (count==360)(count=0);
 
-   for( let i = 0; i < falling.length; i++ ){
-   falling[i].display();
-   falling[i].move();
-  
-}
-  
-  
+  for( let i = 0; i < ball.length; i++ ){
+   ball[i].display();
+   ball[i].move();
+  }
+
 //cursor following
   push();
   fill("#bde0fe")
@@ -88,42 +80,33 @@ function draw() {
   
   //flower4
   push();
- scale(0.8);
-translate(width*0.9,height*1.1);
+  scale(0.8);
+  translate(width*0.9,height*1.1);
   flower(2);
   pop();
  
-  
-  
   Cupunderlayer();
   
   angleMode(DEGREES)
   
   milktea();
-  
- teatop();
-   
+  teatop();
   straw();
- 
   Bubbles();
-if(circleChange== true){ circle(width/1.9,height/1.2,25)
-  circle(width/1.6,height/1,20)
-  circle(width/2.6,height/1.3,30)
-   circle(width/2.8,height/1.03,20)
+    if(circleChange== true){ circle(width/1.9,height/1.2,25)
+      circle(width/1.6,height/1,20)
+      circle(width/2.6,height/1.3,30)
+      circle(width/2.8,height/1.03,20)
      }
   
-  
- cupUpperlayer();
-  
- 
+  cupUpperlayer();
   eyes(1.3);
-  
   mouth();
   
-  myMap = map(mouseX,0,width,0,-20);
+  myMap = map(mouseX,0,width,0,-10);
   
 }
- function mousePressed(){
+function mousePressed(){
    
    if(mouseX>width*0.5 &&
       mouseX<width *0.7 && 
@@ -139,9 +122,7 @@ if(circleChange== true){ circle(width/1.9,height/1.2,25)
               }
 
  } 
-
-
- function mouth (){
+function mouth (){
   push();
   micLevel = mic.getLevel();
   let m = micLevel;
@@ -149,7 +130,6 @@ if(circleChange== true){ circle(width/1.9,height/1.2,25)
   ellipse(width/2, height*0.70, m*600,m*400);
   pop();
 }
-  
 function flower(rot){
  if (colorChange == true){
   push();
@@ -164,12 +144,8 @@ function flower(rot){
   fill(255, 165, 0)
   ellipse(18,4,70,70);
   pop();   
- }
-  
-  
+ } 
 }
-
-
 function milktea(){
     //milktea
   beginShape()
@@ -180,14 +156,12 @@ function milktea(){
   vertex (width*0.3,height*1)
   endShape(close)
 }
-
 function teatop(){
     //tea top  
   ellipse(160,240,125.5,80);
    fill(254,254,254,250)
    ellipse(249,245,106,85);
-}
-  
+} 
 function straw (){
    //Straw
   push();
@@ -201,7 +175,6 @@ function straw (){
   ellipse(265,10,30,5);
   pop();
 }
-
 function Bubbles(){
  //bubbles
   fill(111, 75, 53)
@@ -211,7 +184,6 @@ function Bubbles(){
   circle(180,330,20)
   circle(220,380,25)
 }
-
 function Cupunderlayer(){
    //Cup underlayer
   noStroke();
@@ -223,7 +195,6 @@ function Cupunderlayer(){
   vertex (width*0.3,height*1)
   endShape(close)
 }
-
 function cupUpperlayer(){
   //Cup upperlayer
   push();
@@ -237,7 +208,6 @@ function cupUpperlayer(){
   endShape(close)
   pop(); 
 }
-
 function eyes(){
   //eyes
   push();  
@@ -255,8 +225,5 @@ if(scaleChange == true){
   circle(width*0.39,height*0.55,35)
   circle(width*0.62,height*0.55,35) 
   pop();
-}
-  
-  
 } 
-  
+} 
