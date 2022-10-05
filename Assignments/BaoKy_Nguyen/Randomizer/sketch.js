6b721f682fbd8ec55a0119c559d5d27c3c62280f
Bao <112349630+BaoYky@users.noreply.github.com>
2022-10-05 16:48:41 -0700

    Add sketch.js

---
+++ Assignments/BaoKy_Nguyen/Randomizer/sketch.js
@@ -0,0 +1,11 @@
+//Boba Milkteas
+let milkteas = ["brownSugar","Matcha","thaiTea"]
+
+function setup(){
+  createCanvas(displayWidth,displayHeight);
+  background(255,246,255);
+  console.log(milkteas[1]);
+}
+function draw(){
+  
+}
