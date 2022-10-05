8d668ad6617e904167a00586ea08617e50ab9012
simrannagshain <simran.nagshain@sjsu.edu>
2022-10-05 12:38:08 -0700

    commit

--- Assignments/Simran_Nagshain/Randomizer/2b.js
+++ Assignments/Simran_Nagshain/Randomizer/2b.js
@@ -1,14 +1,22 @@
 //dogs I have loved before
 
-let dogs = ["mowgli", "rico", "scooby", "simba", "coco"];
+let dogs = [{
+    name: "mowgli", color: "blue"
+}, {
+    name: "rico", color: "purple"
+}, {
+    name: "scooby", color: "blue"
+}, {
+    name: "simba", color: "yellow"
+}, {
+    name: "coco", color: "green"
+}];
 
 function setup() {
   createCanvas (600, 600);
   background (200);
 
-
-  console.log(dogs.length);
-
+  console.log(random (4));
 
 }
 
