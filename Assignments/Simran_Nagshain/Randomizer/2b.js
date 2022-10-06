c00c73bb2b889e2b127cdf9557dab05992380b8e
simrannagshain <simran.nagshain@sjsu.edu>
2022-10-06 11:12:03 -0700

    update

--- Assignments/Simran_Nagshain/Randomizer/2b.js
+++ Assignments/Simran_Nagshain/Randomizer/2b.js
@@ -1,7 +1,7 @@
-//dogs I have loved before
+//dog's favorite colors
 
 let dogs = [{
-    name: "mowgli", color: "blue"
+    name: "mowgli", color: "red"
 }, {
     name: "rico", color: "purple"
 }, {
@@ -11,16 +11,25 @@ let dogs = [{
 }, {
     name: "coco", color: "green"
 }];
+let randomIndex;
 
 function setup() {
+
   createCanvas (600, 600);
   background (200);
-
-  console.log(random (4));
+  
+  
 
 }
 
 function draw () {
-    background (200);
+    
 
+}
+function mousePressed () {
+    background (random(200, 225));
+    randomIndex = int(random (dogs.length));
+    text(dogs[randomIndex].name, 50, 50);
+    console.log ("likes", dogs[randomIndex].color);
+    dogs.splice(randomIndex, 1);
 }
