333c002fc00c752988d7cf9502a6a361188f3e0a
cheerchow <80397405+cheerchow@users.noreply.github.com>
2022-10-06 22:56:32 -0700

    Add files via upload

--- Assignments/Tiffany_Chow/randomizer/sketch.js
+++ Assignments/Tiffany_Chow/randomizer/sketch.js
@@ -1,11 +1,49 @@
 // owl
-let owls =["one","two","three","four","five","six"];
+let owls =[{
+  name:"cry",
+  color: "two"
+},{
+  name:"sad",
+  color: "four"
+},{
+  name:"happy",
+  color:"six" 
+}];
 
-funtion setup() {
-    cearteCanvas(600,600);
-    background(200);
+// function setup() {
+//     cearteCanvas(600,600);
+//     background(200);
 
-    console.log(owls.length);
+//     owls[0]= "one"
+//     owls[1]= "two"
+
+//     console.log("initial arry is");
+//     console.log(owls);
+
+//     owls.splice(4,1);
+//     console.log("array after splice")
+//     console.log(owls);
+
+//     owls.unshift("alive");
+//     console.log("array after push")
+//     console.log(owls);
+
+// }
+
+let randomIndex;
+
+function setup() {
+  createCanvas(600,600);
+  background(200);
+
+ 
+ 
+}
+function mousePressed() {
+  background(random(200,255));
+   randomIndex = int(random(owls.length));
+  Text(owls[randomIndex].name,50,50);
+  owls.splice(randomIndex, 1);
 }
 
 function draw(){
