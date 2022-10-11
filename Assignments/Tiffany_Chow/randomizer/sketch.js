4bb3124df34084d702a961f86a3f14ca203af1f0
cheerchow <80397405+cheerchow@users.noreply.github.com>
2022-10-11 12:48:23 -0700

    Add files via upload

--- Assignments/Tiffany_Chow/randomizer/sketch.js
+++ Assignments/Tiffany_Chow/randomizer/sketch.js
@@ -1,37 +1,27 @@
-// owl
-let owls =[{
-  name:"cry",
-  color: "two"
-},{
-  name:"sad",
-  color: "four"
-},{
-  name:"happy",
-  color:"six" 
-}];
+//owls
 let owls = [{
-    name: "sad",
-    color: "red and blue"
+    name: "the sad one",
+    color: "blue and purpule"
   }, {
-    name:"joy",
+    name:"mull",
     color:"pink"
   }, {
-    name: "bonk",
-    color: "white"
+    name: "the happy one",
+    color: "yellow"
   }, {
-    name: "mull",
-    color:"yellow"
+    name: "joy",
+    color:"star"
   }];
   
-
+  let colorful = ["#E6E82D","#FF4F00","#C2FFCD", "#EEC2FF","#EEFFFF"];
   
   let randomIndex;
   
   function setup(){
     createCanvas(displayWidth,displayHeight);
-    background(121,6,20);
-
-// function setup() {
+    background(186, 136, 240 );
+  
+ // function setup() {
 //     cearteCanvas(600,600);
 //     background(200);
 
@@ -50,15 +40,16 @@ let owls = [{
 //     console.log(owls);
 
 // }
-
-function draw(){
-
-}
-
-function mousePressed(){
-  background(random(colorful));
-  randomIndex = int(random(owls.length));
- text(owls[randomIndex].name ,width/3,height*0.2);
-owls.splice(randomIndex,1);
-
-}
+  }
+  
+  function draw(){
+  
+  }
+  
+  function mousePressed(){
+    background(random(colorful));
+    randomIndex = int(random(owls.length));
+   text(owls[randomIndex].name ,width/3,height*0.2);
+   owls.splice(randomIndex,1);
+  
+  }
