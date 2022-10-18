48a1b48c76ef8ff97c595325c9da1b4b977bef1c
Truc Hoang Thanh Tran <truc.h.tran@sjsu.edu>
2022-10-18 13:57:08 -0700

    Update smileBox.js

--- Assignments/Truc_Tran/Randomizer__2.c10/smileBox.js
+++ Assignments/Truc_Tran/Randomizer__2.c10/smileBox.js
@@ -10,6 +10,8 @@ title(){push();
       rect(width*0.06+3, height*.38+3, width*.1, height*.10)
       fill(random(myColors));
       rect(width*0.06, height*.38, width*.1, height*.10)
+      fill("#836953");
+      text("M", width*.07-2, height*.46+1)
       fill('white')
       text("M", width*.07, height*.46)
 
@@ -19,34 +21,41 @@ title(){push();
       rect(width*0.12+3, height*.42+3, width*.1, height*.10)
       fill(random(myColors));
       rect(width*0.12, height*.42, width*.1, height*.10)
+      fill("#836953");
+      text("E", width*.14-2, height*.505+1)
       fill('white')
       text("E", width*.14, height*.505)
 
       rotate(15);
       noStroke();
       fill("#836953");
-      rect(width*0.37+2, height*.32+2, width*.1, height*.10)
+      rect(width*0.37+3, height*.32+3, width*.1, height*.10)
       fill(random(myColors));
       rect(width*0.37, height*.32, width*.1, height*.10)
+      fill("#836953");
+      text("S", width*.386-2, height*.4+1)
       fill('white')
-      text("S", width*.38, height*.4)
+      text("S", width*.386, height*.4)
 
       rotate(-15);
-      fill(random(myColors));
       noStroke();
+      fill("#836953");
+      rect(width*0.4+3, height*.44+3, width*.1, height*.10)
+      fill(random(myColors));
       rect(width*0.4, height*.44, width*.1, height*.10)
       fill('white')
       text("S", width*.415, height*.52)
 
       rotate(15);
-      fill(random(myColors));
       noStroke();
+      fill("#836953");
+      rect(width*0.65+3, height*.27+3, width*.1, height*.10)
+      fill(random(myColors));
       rect(width*0.65, height*.27, width*.1, height*.10)
       fill('white')
       text("A", width*.665, height*.35)
 
       rotate(-15);
-     
       noStroke();
       fill('#836953');
       rect(width*0.68+3, height*.46+3, width*.1, height*.10)
@@ -66,8 +75,8 @@ title(){push();
 
       rotate(-15);
       noStroke();
-     // fill('#836953');
-      //rect(width*.92+3, height*.14+3, width*.1, height*.10)
+     fill('#836953');
+      rect(width*.05+3, height*.55+3, width*.1, height*.10)
       fill(random(myColors));
       rect(width*.05, height*.55, width*.1, height*.10)
       fill('white')
