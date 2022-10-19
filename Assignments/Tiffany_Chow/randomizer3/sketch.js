b43a1709cf082d0645fd9ba965703c77e8b50d38
cheerchow <80397405+cheerchow@users.noreply.github.com>
2022-10-18 23:24:39 -0700

    no

--- Assignments/Tiffany_Chow/randomizer3/sketch.js
+++ Assignments/Tiffany_Chow/randomizer3/sketch.js
@@ -1,71 +1,96 @@
-//owls
+//owls hooty being crazy 
 let owls = [{
     name: "the sad one",
-    color: "blue and purpule"
+    do: "run" ,
+    color: "red"
   }, {
     name:"mull",
+    do: "play" ,
     color:"pink"
   }, {
     name: "the happy one",
-    color: "yellow"
+    do: "jump" ,
+    color: "blue"
   }, {
     name: "joy",
-    color:"star"
+    do: "fly" ,
+    color:"green"
   }];
   
-  let star = ["#E6E82D","#FF4F00","#C2FFCD", "#EEC2FF","#EEFFFF"];
+  // let star = ["#E6E82D","#FF4F00","#C2FFCD", "#EEC2FF","#EEFFFF"];
   
   let randomIndex;
   let animating = false;
   let hootys = [];
+  letimageCounter = 0;
+  let button;
 
   function preload(){
     for (let i = 0; i <= 13; i++){
-      hootys[i] = loadImage(`image/hooty_${1}.JPG`)
+      hootys[i] = loadImage('image/hooty_'+i+'.jpg')
     }
   }
 
   function setup(){
-    createCanvas(displayWidth,displayHeight);
-    background(186, 136, 240 );
-    textSize(50);
+    createCanvas(200,200);
+    background(200);
+
     imageMode(CENTER);
-    frameRate(12);
-   
-
-    text("click to randomize, width/2, height/2");
+    frameRate(9);
+  
+    text ("click to randomize",20, 20);
      console.log(hootys);
+     button = createButton("click to randomize");
+     button.mousePressed(buttonPressed);
+  }
 
-function draw(){
+function draw () {
  
 
   if(animating == true){
      clear();
-   Image(hootys[imageCounter],width/2, height/2);
+   image(hootys[imageCounter],width/2, height/2);
 
-   if (imageCounter < hootys.length - 1)
+   if (imageCounter < hootys.length - 1){
    imageCounter++;
    console.log(imageCounter); 
   }else{
     imageCounter = 0;
   }
 }
-   
-  function randomizer(){
-    animating = false;
+}
+
+function randomizer (){
+  animating = false;
+ 
+  clear();
+  randomIndex = int(random (owls.length));
+
+  image(random(hootys),width/2, height/2);
+  textSize (32);
+  text(`${owls[randomIndex].name} will ${owls[randomIndex].do}`, width/6, height - 150);
+  fill(owls[randomIndex].color);
+}
+
+
+  // function randomizer(){
+  //   animating = false;
   
-    if (owls[0]){
-    background(0);
-    clear();
-    randomIndex = int(random(owls.length));
-   text(owls[randomIndex].name,50,50);
-   image(random(hootys), width/2,height /2);
-   owls.splice(randomIndex,1);
-  } else {
-    background(0);
-    text("nothing left!", width/2, height*0.2);
-    }
-  }
+  //   if (owls[0]){
+  //   background(0);
+  //   clear();
+  //   randomIndex = int(random(hootys.length));
+  //  text(owls[randomIndex].name,50,50);
+  //  image(random(hootys), width/2,height /2);
+  //  owls.splice(randomIndex,1);
+  // } else {
+  //   background(0);
+  //   text("nothing left!", width/2, height*0.2);
+  //   }
+  // }
+
+
+  
   //  function randomizer(){
   //   animating = false;
   
@@ -81,8 +106,7 @@ function draw(){
   //   text("nothing left!", width/2, height*0.2);
   //   }
   // }
-  
-  function mousePressed(){
+    function buttonPressed () {
     animating = true;
     setTimeout(randomizer,2000);
   
