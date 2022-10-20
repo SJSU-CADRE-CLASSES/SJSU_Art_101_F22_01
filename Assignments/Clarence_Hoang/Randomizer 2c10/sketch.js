0bb2c41896e88642ea35e1b984d958d238f56797
Clarence Hoang <clarence.hoang@sjsu.edu>
2022-10-20 06:26:59 -0700

    dom to 2c10

--- Assignments/Clarence_Hoang/Randomizer 2c10/sketch.js
+++ Assignments/Clarence_Hoang/Randomizer 2c10/sketch.js
@@ -9,36 +9,44 @@ let bestFriends = [
 let randomIndex;
 let animating = false;
 let friends = [];
+let imageCounter= 0;
+let button;
 
-// function preload(){
-//   for (let i = 0; i <= 6; i++){
-//     friends[i] = loadImage(`assets/IMG_${i}.JPG`);
-//   }
-// }
+function preload(){
+  for (let i = 0; i <= 5; i++){
+    friends[i] = loadImage(`assets/friend_${i}.jpg`);
+  }
+}
 
 function setup() {
   createCanvas(400, 400);
   background(200);
   textSize(15);
+  imageMode(CENTER);
   text("My Best Friends",50,50);
   text("Click to randomize",50,200);
+  button = createButton("Click to randomize");
+  button.mousePressed(buttonPressed);
   y = 0;
-  console.log(friends);
 }
 
 function draw() {
-  //let rightWall = width;
-  //let xc = constrain();
   resetBar();
-  // if(animating ==true){
-  //   image(friends[0],0,0)
-  // }
+  if(animating ==true){
+    image(friends[imageCounter],width/2,height/2)
+    if(imageCounter < friends.length - 1){
+      imageCounter++;
+    } else {
+      imageCounter = 0;
+    }
+  }
 }
 
 function resetBar(){
 
   fill(50,50,255)
   if(animating == true){
+    clear();
   rect(0,height-20,y,20);
   } else{
   y = 1;
@@ -52,16 +60,17 @@ function randomizer(){
   if(bestFriends[0]){
   background(random(200,255));
   randomIndex = int(random(bestFriends.length));
-  text(`${bestFriends[randomIndex].name}'s favorite color is ${bestFriends[randomIndex].color}`,50,50);  
   // text(bestFriends[randomIndex].name +"'s favorite color is " + bestFriends[randomIndex].color,50,50);
-  bestFriends.splice(randomIndex,1);
+  //bestFriends.splice(randomIndex,1);
+  image(random(friends),width/2,height/2)
+  text(`${bestFriends[randomIndex].name}'s favorite color is ${bestFriends[randomIndex].color}`,50,50); 
 } else {
   background(random(200,255));
   text("Find more friends!",50,50);
 }
 }
 
-function mousePressed(){
+function buttonPressed(){
   animating = true;
   setTimeout(randomizer,1000);
 }
