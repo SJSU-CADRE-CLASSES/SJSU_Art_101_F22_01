23ca01a5e415386d1181d4f10157b442480902cb
Tonybaloni408 <43894536+Tonybaloni408@users.noreply.github.com>
2022-10-24 20:12:32 -0700

    pushed randomizer

---
+++ Assignments/Antonio_Plancarte/Randomizer/sketch.js
@@ -0,0 +1,111 @@
+let catImg =[];
+
+//types of cats
+
+function preload() {
+    
+  
+   
+  
+    for(let i = 1; i <= 5; i++){
+      catImg[i] = loadImage("assets/cat"+ i+".jpg"); 
+    }
+}
+
+
+let cats = 
+
+[{
+    
+    breed:"American Shorthair", 
+    age: "Kitten",
+    pic: catImg[2]
+}, 
+
+{
+    
+    breed:"British Shorthair", 
+    age: "Adult",
+    pic: catImg[3]
+}, 
+{
+    
+    breed:"Sphynx", 
+    age:"Senior",
+    pic: catImg[4]
+}, 
+{
+    
+    breed:"Russian Blue", 
+    age: "Kitten",
+    pic: catImg[5]
+},
+{
+    
+    breed:"Domestic Shorthair", 
+    age: "Senior",
+    pic: catImg[1]
+}];
+
+let randomIndex;
+let animating = false;
+
+
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+    textSize(32);
+    
+    text("click to randomize", 50, 50 );
+    
+    
+    //console.log(cats.length)
+    //console.log(cats[randomIndex].breed);
+    //console.log(cats);
+    //console.log(cats[3].age);
+    //console.log(cats[int(random(3))]);
+
+}
+
+    function draw() {
+if(animating== true){
+    ellipse(random(width), random(height), random(50, 200));
+}
+
+    }
+   
+function randomizer(){
+    animating = false;
+
+         if(cats[0]){
+            //this displays random name and splices it out of array
+         background (random(200, 255));
+        
+        
+         randomIndex= int(random(cats.length));
+        //text(`cats[randomIndex].breed} 's lucky day) 
+       
+        image(cats[randomIndex].pic, 300, 300, 256, 362);
+       
+        //Age: ${cats[randomIndex].age}`, 50, 50);
+          cats.splice(randomIndex, 1);
+        //text(cats[randomIndex].breed + "'s lucky day Age: " + cats[randomIndex].age, 50, 50);
+          //cats.splice(randomIndex, 1);
+
+        }
+        else {
+
+         background (random(200, 255));
+         text("nothing here!", 50, 50);
+        }
+        
+ }
+
+   
+   
+   
+    function mousePressed(){
+        animating = true;
+        setTimeout (randomizer, 2000);
+    
+}
