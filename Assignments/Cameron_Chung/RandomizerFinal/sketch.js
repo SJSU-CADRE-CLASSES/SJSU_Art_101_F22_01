675648653216ddea6fb3677619e6c35d46d78276
CameronChung <73194885+CameronChung@users.noreply.github.com>
2022-10-25 14:39:06 -0700

    fix

--- Assignments/Cameron_Chung/RandomizerFinal/sketch.js
+++ Assignments/Cameron_Chung/RandomizerFinal/sketch.js
@@ -4,18 +4,13 @@ let randomIndex;
 let button;
 let shapes = [];
  
-shapes[0] = "heart";
-shapes[1] = "circle";
-shapes[2] = "square";
-shapes[3] = "triangle";
-//console.log(shapes);
+
+shapes = ['Draw a heart', 'Draw a circle', 'Draw a square', 'Draw a triangle']
 
 function preload() {
   ready = loadImage("assets/sanrio_0.png");
   draft = loadImage("assets/background.jpg");
-//   for (let i = 0; i <= 3; i++){
-//     shapes[i] = text(`${shapes[randomIndex]}`, 50, 50);
-// }
+
 }
 
 function setup() {
@@ -23,7 +18,9 @@ function setup() {
   textSize(15);
   image(ready, 0, 0);
   image(draft, 0, 0);
-  text('''+shapes[randomIndex]+''', 50, 50);
+  randomIndex = int(random(shapes.length)-1);
+  text(str(shapes[randomIndex]), 50, 50);
+  console.log(shapes[0]);
   clearButton = createButton('click to change prompt');
   clearButton.mousePressed(clearScreen);
 }
@@ -32,9 +29,8 @@ function randomizer(){
     animating = false;
     if(shapes[0]){
     clear();
-    // background(random(200, 255));
-    randomIndex = int(random(shapes.length));
-    //text(shapes[randomIndex], 50, 50);
+    randomIndex = int(random(shapes.length)-1);
+    text(shapes[randomIndex], 50, 50);
     console.log(shapes[0])
     shapes.splice(randomIndex, 1);
     } else{
@@ -45,10 +41,10 @@ function randomizer(){
 
 function clearScreen() {
   animating = true;
-  clear();
+  randomIndex = int(random(shapes.length)-1);
   image(ready, 0, 0);
   image(draft, 0, 0);
-  //text(`${shapes[randomIndex]}`, 50, 50);
+  text(shapes[randomIndex], 50, 50);
 }
 
 function mouseDragged() {
